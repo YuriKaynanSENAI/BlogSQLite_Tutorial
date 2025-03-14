128eeab0d915b154691ba2426bc83b7e747774ee
Yuri_Puyu <yurikaynandesouzanogueira@gmail.com>
2025-03-14 13:14:47 -0300

    Atualização do código 14/03

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const PORT = 8000; // Porta TCP do servidor HTTP da aplicação
 
 const app = express();
 
+/*Método express.get necessita de dois parâmetros, na ARROW FUNCTION, o primeiro  são os dados de servidor (REQUISITION - 'red')
+o segundo, são os dados que serão enviados ao cliente (RESULT - 'res')*/
+
+app,
+  get("/", (req, res) => {
+    res.send("Helo word!");
+  });
+
+app.get("/sobre", (req, res) => {
+  res.send("Página sobre");
+});
+
+pp.get("//info", (req, res) => {
+  res.send("Página informação");
+});
+
 //app.listen() deve ser o último comando da aplicação(app.js)
 app.listen(8080, () => {
   console.log(`servidor sendo executado na ${PORT}!`);

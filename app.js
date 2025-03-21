0051b5cb6f9a4d22485c19e1ff0add96ffa3d67e
Yuri_Puyu <yurikaynandesouzanogueira@gmail.com>
2025-03-21 10:17:41 -0300

    Atualização app.js

--- app.js
+++ app.js
@@ -1,26 +1,67 @@
-const express = require("express");
+const express = require("express"); // Importa lib do Express
+const sqlite3 = require("sqlite3"); // Importa lib do sqlite3
 
 const PORT = 8000; // Porta TCP do servidor HTTP da aplicação
 
-const app = express();
+const app = express(); // Instância para uso do Express
 
-/*Método express.get necessita de dois parâmetros, na ARROW FUNCTION, o primeiro  são os dados de servidor (REQUISITION - 'red')
-o segundo, são os dados que serão enviados ao cliente (RESULT - 'res')*/
+const db = new sqlite3.Database("user.db"); // Instância para uso do Sqlite3, e usa o arquivo 'user.db'
 
-app,
-  get("/", (req, res) => {
-    res.send("Helo word!");
-  });
+db.serialize(() => {
+  // Este método permite enviar comandos SQL em modo 'sequencial'
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+  );
+});
 
+// Cria conexão com o banco de dados
+
+const index =
+  "<a href='/home'> Home</a><a href='/sobre'> Sobre</a><a href='/login'> Login</a><a href='/cadastro'> Cadastro</a><a href='/info'> Info</a>";
+const home = 'Vc está na página "Home"<br><a href="/">Voltar</a>';
+const sobre = 'Vc está na página "Sobre"<br><a href="/">Voltar</a>';
+const login = 'Vc está na página "Login"<br><a href="/">Voltar</a>';
+const cadastro = 'Vc está na página "Cadastro"<br><a href="/">Voltar</a>';
+const info = 'Vc está na página "Info"<br><a href="/">Voltar</a>';
+
+/* Método express.get necessita de dois parâmetros
+// Na ARROW FUNCTION: o primeiro são os daods do servidor (REQUISITION - 'res'):
+o segundo, são os dados que serão enviados ao cliente (RESULT - 'res') */
+app.get("/", (req, res) => {
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000
+  res.send(index);
+});
+
+app.get("/home", (req, res) => {
+  res.send(home);
+});
+
+// Programação de rotas do método GET do HTTP 'app.get()'
 app.get("/sobre", (req, res) => {
-  res.send("Página sobre");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/sobre
+  res.send(sobre);
 });
 
-pp.get("//info", (req, res) => {
-  res.send("Página informação");
+app.get("/login", (req, res) => {
+  res.send(login);
 });
 
-//app.listen() deve ser o último comando da aplicação(app.js)
-app.listen(8080, () => {
-  console.log(`servidor sendo executado na ${PORT}!`);
+app.get("/cadastro", (req, res) => {
+  res.send(cadastro);
 });
+
+app.get("/info", (req, res) => {
+  res.send(info);
+});
+
+// app.get("/info", (req, res) => {
+//   // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/info
+//   res.send(info);
+// });
+
+// app.listen() deve ser o último comando da aplicação (app.js)
+app.listen(PORT, () => {
+  console.log(`Servidor sendo executado na porta ${PORT}!`);
+});
+
+//dia 21/03/2025 atualizei os códigos usando as anotações do Ribeiro

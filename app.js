7ec0c30ad9a9927e6d91c48c08f1f463ba822562
Yuri_Puyu <yurikaynandesouzanogueira@gmail.com>
2025-03-21 13:58:58 -0300

    ajuste de rotas e links

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const info = 'Vc está na página "Info"<br><a href="/">Voltar</a>';
 o segundo, são os dados que serão enviados ao cliente (RESULT - 'res') */
 app.get("/", (req, res) => {
   // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000
-  res.send(index);
+  // res.send(index);
+  res.render("index");
 });
 
 app.get("/home", (req, res) => {
